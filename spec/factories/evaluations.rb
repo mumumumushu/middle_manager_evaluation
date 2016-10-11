b007e16342ce250bd25f80e5b9af75b6b4e5b08c
Ivan Lan <mumumumushu@gmail.com>
2016-10-11 11:02:29 +0800

    add apitome for api doc optimize

--- spec/factories/evaluations.rb
+++ spec/factories/evaluations.rb
@@ -6,11 +6,11 @@
 #  thought_morals         :text
 #  upright_incorruptiable :text
 #  duties                 :text
-#  evaluation             :integer
 #  user_type              :string
 #  user_id                :integer
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  evaluation_totality    :integer
 #
 
 FactoryGirl.define do
