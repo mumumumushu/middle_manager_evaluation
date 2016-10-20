29e6640ef2f356d65d5bac0e353696a45d3db233
ivan Lan <mushu@ACA8010D.ipt.aol.com>
2016-10-20 22:32:16 +0800

    change some setting in Gemfile and config/appliaction.rb

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,14 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 require 'multi_json'
 
@@ -28,19 +28,19 @@ module MiddleManagerEvaluation
     # -- all .rb files in that directory are automatically loaded.
 
     # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # # Middleware like session, flash, cookies can be added back manually.
+    # # Skip views, helpers and assets when generating a new resource.
+    # config.api_only = true
 
-    # Rack::Cors provides support for Cross-Origin Resource Sharing (CORS) for Rack compatible web applications.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
+    # # Rack::Cors provides support for Cross-Origin Resource Sharing (CORS) for Rack compatible web applications.
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', headers: :any, methods: [:get, :post, :options]
+    #   end
+    # end
 
-    config.action_controller.asset_host = "http://middle_manager_evaluation.com"
+    # config.action_controller.asset_host = "http://middle_manager_evaluation.com"
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
