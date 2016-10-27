7fcb5d28c40032c0587f4cef8df4c9fe6d1dfb03
ivan Lan <mushu@ivandeMacBook-Pro.local>
2016-10-27 18:04:47 +0800

    imporve the code with map

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -15,7 +15,8 @@ class Activity < ApplicationRecord
 	before_save :set_created_year
 	has_many :self_evaluations
 
-	after_create :create_self_evaluation
+	# after_create :create_self_evaluation  #加上后 factory girl 的数据不启用  统计结果为空
+																				#去掉后  出现统计结果
 	
 private
 
@@ -23,7 +24,7 @@ private
 		self.activity_created_year = self.first_phase_begin.year
 	end
 
-	def create_self_evaluation
+	def create_self_evaluation  
 		MiddleManager.where("take_part_in = ?", true).each do |middle_manager|
 			_self_evaluation = SelfEvaluation.new(:activity_id => self.id,
 																						:middle_manager_id => middle_manager.id,
