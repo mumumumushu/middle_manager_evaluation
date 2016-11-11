28fc5d64d0c401993bb97a422e940b5b5f6975dc
Ivan Lan <mumumumushu@gmail.com>
2016-11-11 10:11:06 +0800

    fix bug in the last commi

--- db/migrate/20161111014728_add_third_phase_end_to_activity.rb
+++ db/migrate/20161111014728_add_third_phase_end_to_activity.rb
@@ -1,5 +1,5 @@
 class AddThirdPhaseEndToActivity < ActiveRecord::Migration[5.0]
   def change
-  	add_column :activities, :third_phase_end, :datatime
+  	add_column :activities, :third_phase_end, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20161111014728) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.integer  "activity_created_year"
-    t.time     "third_phase_end"
+    t.datetime "third_phase_end"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20161111014728) do
     t.datetime "updated_at",               null: false
     t.integer  "created_year"
     t.integer  "activity_id"
+    t.         "activity"
     t.string   "user_info"
     t.text     "evaluated_user_info"
     t.index ["activity_id"], name: "index_self_evaluations_on_activity_id"
@@ -96,20 +97,20 @@ ActiveRecord::Schema.define(version: 20161111014728) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "encrypted_password",                default: "", null: false
+    t.string   "encrypted_password",                default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
-    t.string   "job_num",                           default: "", null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "job_num",                           default: "",    null: false
     t.string   "authentication_token",   limit: 30
-    t.boolean  "take_part_in"
+    t.boolean  "take_part_in",                      default: false
     t.string   "user_type"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
