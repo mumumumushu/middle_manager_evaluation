2cb980d9a5ddfb9ca8fd44f845eb2ebec0905557
ivan Lan <mushu@ACA80106.ipt.aol.com>
2016-11-17 11:06:12 +0800

    add get_home_info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,1 +1,13 @@
-home_controller.rb
+class HomeController < ApplicationController
+
+	def get_home_info
+		@activity = Activity.last
+		if Time.now >= @activity.first_phase_begin && Time.now <= @activity.third_phase_end
+			respond_with @activity, template: 'activities/activity', status: 200
+		else
+			@error = { error: "最新的考核活动未上线"}
+			respond_with @error, template: 'error'
+		end
+	end
+
+end
