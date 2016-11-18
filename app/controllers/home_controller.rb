78c6580439fb3f6d2e4f4e40eabd29b8429fef35
Ivan Lan <mumumumushu@gmail.com>
2016-11-18 16:35:49 +0800

    remove validating from home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
 	def get_home_info
 		@activity = Activity.last
-		if Time.now >= @activity.first_phase_begin && Time.now <= @activity.third_phase_end
+		# if Time.now >= @activity.first_phase_begin && Time.now <= @activity.third_phase_end
 			respond_with @activity, template: 'activities/activity', status: 200
-		else
-			@error = { error: "最新的考核活动未上线"}
-			respond_with @error, template: 'error'
-		end
+		# else
+		# 	@error = { error: "最新的考核活动未上线"}
+		# 	respond_with @error, template: 'error'
+		# end
 	end
 
 end
