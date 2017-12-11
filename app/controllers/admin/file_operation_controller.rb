38ed64cc2b5b97de3e2269ab3cca45039726dc4d
ivan Lan <mumumumushu@gmail.com>
2017-12-11 13:46:24 +0800

    Fix Admin::FileOperationController#create_file_name if the dir does not exist.

--- app/controllers/admin/file_operation_controller.rb
+++ app/controllers/admin/file_operation_controller.rb
@@ -51,9 +51,11 @@ class Admin::FileOperationController < ActionController::Base
 private
   def create_file_name #only .xls
     _filename = ""
+    _dir = "pubilc/file/"
     chars = ('a'..'z').to_a + ('0'..'9').to_a
     1.upto(6) { |i| _filename << chars[rand(chars.size-1)] }
-    if File.exist?(File.join("pubilc/file/","#{_filename}.xls"))
+    FileUtils.mkdir(_dir) unless File.exist?(_dir)
+    if File.exist?(File.join(_dir,"#{_filename}.xls"))
       create_file_name
     end
     "/home/edu/middle_manager_evaluation/pubilc/file/" +  _filename + '.xls'
